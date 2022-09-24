d6ae5db8f5aa3f4a6031bdc0aa55444a34bda449
italo <italo.rgrego@gmail.com>
2022-09-24 16:19:14 -0300

    novos arquivos 2

---
+++ assets/js/be86a017.a8b3c935.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[386],{5424:e=>{e.exports=JSON.parse('{"title":"Materiais","slug":"/category/materiais","permalink":"/proji-4.0/docs/category/materiais","navigation":{"previous":{"title":"Introdu\xe7\xe3o","permalink":"/proji-4.0/docs/"},"next":{"title":"Materiais","permalink":"/proji-4.0/docs/materiais/"}}}')}}]);
